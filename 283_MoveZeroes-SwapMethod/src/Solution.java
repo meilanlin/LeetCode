88d5f967c40e3eca124b1c10e669cf86e0da3180
Zonglin Li <victorlizonglin@gmail.com>
2019-01-16 16:24:27 -0600

    Add files via upload
    leetcode 283

---
+++ 283_MoveZeroes-SwapMethod/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+	public void moveZeroes(int[] nums) {
+		if (nums == null || nums.length <= 1) {
+			return;
+		}
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				int temp = nums[index];
+				nums[index] = nums[i];
+				nums[i] = temp;
+				index++;
+			}
+		}
+	}
+}
